b8d8d9107284981937cbb1ffd93307c0fb1758bb
sweaterr <sweaterr@gmail.com>
2023-06-22 14:47:25 +0900

    torchrun

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,8 +6,11 @@ GPUS=$2
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+#python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
 
 #/usr/local/bin/python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
 #/usr/local/bin/python $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+python -m torchrun --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+#torchrun --nproc_per_node=2
