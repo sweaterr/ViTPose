ddbba9a2a9e45b2e0300998b62cbeaad6362b101
sweaterr <sweaterr@gmail.com>
2023-06-22 14:41:58 +0900

    /usr/local/bin/python

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,8 +6,8 @@ GPUS=$2
 PORT=${PORT:-29500}
 
 #PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-#/usr/local/bin/python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
 #    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
 
-/usr/local/bin/python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+#/usr/local/bin/python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+/usr/local/bin/python $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
