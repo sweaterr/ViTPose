5a93db7f0e81f6cde28733c8579e330125282d51
sweaterr <sweaterr@gmail.com>
2023-06-23 09:30:02 +0900

    /content/drive//MyDrive/dataset/coco2017

--- configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_small_coco_256x192_colab.py
+++ configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_small_coco_256x192_colab.py
@@ -140,7 +140,7 @@ val_pipeline = [
 test_pipeline = val_pipeline
 
 
-data_root = '/content/coco2017'
+data_root = '/content/drive//MyDrive/dataset/coco2017'
 data = dict(
     samples_per_gpu=64,
     workers_per_gpu=4,
